18a0fc97b414bed7f273daf76b8c4ee8c815ac66
HXY <63409863+Cassie-cpu@users.noreply.github.com>
2020-07-28 17:30:23 +0800

    Create index_twelve.vue

---
+++ src/view/main/index_twelve.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <el-aside>
+          <template>
+            <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+                <el-tab-pane label="用户管理" name="first">描述说明</el-tab-pane>
+            </el-tabs>
+          </template>
+        </el-aside>
+        <el-footer>
+            <el-row>
+                <el-button type="primary">上一步</el-button>
+                <el-button type="primary">下一步</el-button>
+            </el-row>
+        </el-footer>
+    </div>
+</template>
