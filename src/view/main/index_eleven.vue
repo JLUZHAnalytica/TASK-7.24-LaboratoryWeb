d878fbc051a69f2a9701e2a46c9689ec5901201b
HXY <63409863+Cassie-cpu@users.noreply.github.com>
2020-07-28 17:30:09 +0800

    Create index_eleven.vue

---
+++ src/view/main/index_eleven.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-button type="text" @click="open">买入</el-button>
+</template>
+
+<script>
+  export default {
+    methods: {
+      open() {
+        this.$alert('将投入100000元申购该投资组合', '标题名称', {
+          confirmButtonText: '确定买入',
+          callback: action => {
+            this.$message({
+              type: 'info',
+              message: `action: ${ action }`
+            });
+          }
+        });
+      }
+    }
+  }
+</script>
