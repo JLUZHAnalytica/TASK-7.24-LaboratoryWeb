cb6882d788f7d0fa83a809ec8c775ff5fac19c98
HXY <63409863+Cassie-cpu@users.noreply.github.com>
2020-07-28 17:30:26 +0800

    Create index_thirteen.vue

---
+++ src/view/main/index_thirteen.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-button type="text" @click="open">点击打开 Message Box</el-button>
+</template>
+
+<script>
+  export default {
+    methods: {
+      open() {
+        this.$alert('图片', '调仓建议', {
+          confirmButtonText: '调仓',
+          confirmButtonText: '不调仓',
+          callback: action => {
+            this.$message({
+              type: 'info',
+              message: `action: ${ action }`
+            });
+          }
+        });
+      }
+    }
+  }
+</script>
