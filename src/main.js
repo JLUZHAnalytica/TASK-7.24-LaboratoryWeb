08ee2aedb79c16fb157860ddc9f8697a84ecfed7
Weihong-Liu <63888590@qq.com>
2020-07-29 15:26:12 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css'
+import { codemirror } from 'vue-codemirror'
+import 'codemirror/lib/codemirror.css'
+Vue.use(codemirror)
 Vue.config.productionTip = false
 Vue.use(element)
 new Vue({
