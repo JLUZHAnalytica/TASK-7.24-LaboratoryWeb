057669345f6f7aa3e69fa92c96e4ed7b5d39bf7a
Yokey <zyf6@outlook.com>
2020-08-03 17:17:48 +0800

    Create jbbook.vue

---
+++ src/components/code/jbbook.vue
@@ -0,0 +1,15 @@
+<template>
+  <iframe
+    id="tabiframe"
+    src="http://121.199.60.137:8888/notebooks/python%E5%AE%9E%E9%AA%8C%E5%AE%A4.ipynb"
+    width="100%"
+    height="1000px"
+  ></iframe>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
